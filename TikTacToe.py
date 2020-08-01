3d8c9fed9da5aaf317287b1c456a8ecafd35db14
Edex01 <jorasiac@gmail.com>
2020-08-01 10:00:01 +0530

    IsWinner function added

--- TikTacToe.py
+++ TikTacToe.py
@@ -25,4 +25,15 @@ def printBoard():
     print('--------')
     print(' ' + board[7] + '|' + board[8] + '|' +board[9])
 
+# check winner
+def IsWinner(b,l):
+    return ((b[1] == l and b[2] == l and b[3] == l) or
+    (b[4] == l and b[5] == l and b[6] == l) or
+    (b[7] == l and b[8] == l and b[9] == l) or
+    (b[1] == l and b[4] == l and b[7] == l) or
+    (b[2] == l and b[5] == l and b[8] == l) or
+    (b[3] == l and b[6] == l and b[9] == l) or
+    (b[1] == l and b[5] == l and b[9] == l) or
+    (b[3] == l and b[5] == l and b[7] == l))
+
 printBoard()
