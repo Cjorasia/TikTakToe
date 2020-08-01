5ccb0a31ae3c0a7de04c1d0bfc03fb65d13b212b
Edex01 <jorasiac@gmail.com>
2020-08-01 09:55:55 +0530

    SpaceIsFree function added

--- TikTacToe.py
+++ TikTacToe.py
@@ -1,9 +1,15 @@
+# Board Boxes
 board = [' ' for i in range(10)]
 
-
+# insert in boxes
 def insertLetter(Letter, pos):
     board[pos]=Letter
 
+# check if boxes free
+def spaceIsFree(pos):
+    return board[pos] == ' '
+
+# how board looks
 def printBoard():
 
     print(' ' + board[1] + '|' + board[2] + '|' + board[3])
